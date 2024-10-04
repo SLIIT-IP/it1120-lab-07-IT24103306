ae5768fb20fb15b447d64ceb84eb1f8fca2c58c4
IT24103306 <it24103306@my.sliit.lk>
2024-10-04 09:57:49 +0530

    Add IT24103306.Lab7Q2A.java

---
+++ Q2/IT24103306.Lab7Q2A.java
@@ -0,0 +1,18 @@
+public class IT24103306Lab7Q2A{
+	public static void main(String[]args){
+
+     int i;
+     int j; 
+     
+     for(i=1; i <=4; i++)//loop for the rows//
+     {
+	for(j=1; j <=5; j++)// loop for the column//
+	{
+	    System.out.print(" "+"$");
+ 	}
+
+	System.out.print("\n");
+
+       }
+     }
+}
